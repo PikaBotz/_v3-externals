c83bac535425a610077b5a793834d25fd0692f44
Pika-Kun 3 <121213527+PikaBotz@users.noreply.github.com>
2026-01-27 00:15:58 +0530

    Add test2 module for testing static imports

---
+++ _test/_test2.js
@@ -0,0 +1,37 @@
+// "for:anya.v3"
+
+import { cmd } from "../lib/plugins.js";
+
+import { getSecretMessage, calculateMath } from "../lib/_test.helper.js";
+
+export const install = {
+
+    module: "test-package-v2",
+
+    files: [
+        {
+
+            url: "https://raw.githubusercontent.com/PikaBotz/_v3-externals/refs/heads/main/_Helper/_test.helper.js", 
+
+            path: "./lib/_test.helper.js",
+
+            force: true 
+        }
+    ]
+};
+
+export default [
+    cmd({
+        name: "testexternal2",
+        category: "owner",
+        desc: "Tests static dependency imports.",
+        react: "🔗"
+    }, async (c, msg) => {
+
+        const msg1 = getSecretMessage();
+
+        const msg2 = calculateMath(10, 50);
+
+        return msg.reply(`${msg1}\n\n${msg2}\n> 2`);
+    })
+];
