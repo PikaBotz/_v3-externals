cf5bb0a5ff9337398fec573bdfa9f94a0725595c
Pika-Kun 3 <121213527+PikaBotz@users.noreply.github.com>
2026-08-14 23:16:39 +0530

    Refactor imports and update video fetching logic

--- downloader/tikporn.js
+++ downloader/tikporn.js
@@ -1,30 +1,36 @@
 // "for:anya.v3"
 
-import { cmd } from "../../lib/plugins.js";
+import Cg from "../../config.js";
 
-import { getTikPornVideos } from "../../lib/downloader/tik-porn.core.js";
+import {
+    cmd,
+    func
+} from "../../lib/index.js";
+
+import {
+    getTikPornVideos
+} from "../../lib/downloader/tik-porn.core.js";
 
 export const install = {
 
     module: "tik-porn-package-v1",
 
-    files: [
-        {
+    files: [{
 
-            url: "https://raw.githubusercontent.com/PikaBotz/_v3-externals/refs/heads/main/_Helper/downloader/tik-porn.core.js", 
+            url: "https://raw.githubusercontent.com/PikaBotz/_v3-externals/refs/heads/main/_Helper/downloader/tik-porn.core.js",
 
             path: "./lib/downloader/tik-porn.core.js",
 
-            force: true 
+            force: true
         },
-        
-                {
 
-            url: "https://raw.githubusercontent.com/PikaBotz/_v3-externals/refs/heads/main/_Helper/downloader/tik-porn.scrape.py", 
+        {
+
+            url: "https://raw.githubusercontent.com/PikaBotz/_v3-externals/refs/heads/main/_Helper/downloader/tik-porn.scrape.py",
 
             path: "./lib/downloader/tik-porn.scrape.py",
 
-            force: true 
+            force: true
         }
     ]
 };
@@ -37,10 +43,31 @@ export default [
         desc: "Get tiktok size short porn videos.",
         react: "😋"
     }, async (c, msg) => {
+        if (msg.isGroup && !group.nsfw) return {
+            executed: false,
+            metadata: await msg.reply(Cg.RESPONSE.nsfwDisabled)
+        }
+        const {
+            key
+        } = await msg.reply(Cg.RESPONSE.wait);
+
+        const randomVideo = await getTikPornVideos({
+            page: "random",
+            limit: 1
+        });
+
+        const metadata = await msg.reply({
+            video: {
+                url: randomVideo[0].url
+            },
+            caption: `> _Size: ${randomVideo[0].size}_`
+        });
+
+        await msg.delete(key);
 
         return {
-        executed: true,
-        metadata: await msg.reply("Working...")
+            executed: true,
+            metadata
         }
     })
 ];
